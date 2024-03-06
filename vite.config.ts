654224f22a4293e9efeee259dd419a9ce4ccca26
joaopedro-objctv <joao.pedro@objctv.one>
2024-03-06 12:11:13 -0300

    <BUILD> [Project] : Changed base url in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meu_portifolio/',
+  base: '/meu-portifolio/',
   plugins: [
     vue(),
   ],
